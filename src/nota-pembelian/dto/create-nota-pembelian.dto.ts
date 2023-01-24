9bb99eac9f79f0a32becc3760f00c2189fcbc094
Muh-Baharuddin <4519210022@univpancasila.ac.id>
2023-01-24 21:00:08 +0700

    create dto with validator for nota pembelian, create nota pembelian repository and create findAll nota pembelian

--- src/nota-pembelian/dto/create-nota-pembelian.dto.ts
+++ src/nota-pembelian/dto/create-nota-pembelian.dto.ts
@@ -1,1 +1,16 @@
-export class CreateNotaPembelianDto {}
+import { IsNotEmpty, IsDate } from 'class-validator';
+
+export class CreateNotaPembelianDto {
+  @IsNotEmpty({ message: 'tanggal tidak boleh kosong' })
+  @IsDate()
+  tanggal: Date;
+
+  @IsNotEmpty({ message: 'supplier tidak boleh kosong' })
+  supplier: string;
+
+  @IsNotEmpty({ message: 'barang tidak boleh kosong' })
+  barang: string;
+
+  @IsNotEmpty({ message: 'biaya tidak boleh kosong' })
+  biaya: number;
+}
---
+++ src/nota-pembelian/nota-pembelian.repository.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@nestjs/common';
+import { DataSource, Repository } from 'typeorm';
+import { NotaPembelian } from './entities/nota-pembelian.entity';
+
+@Injectable()
+export class NotaPembelianRepository {
+  private repository: Repository<NotaPembelian>;
+
+  constructor(private dataSource: DataSource) {
+    this.repository = this.dataSource.getRepository(NotaPembelian);
+  }
+
+  findAllNotaPembelian(): Promise<NotaPembelian[]> {
+    return this.repository.find();
+  }
+}
