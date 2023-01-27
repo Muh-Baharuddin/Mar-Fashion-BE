b264ef7992803834c13a1dab7b95802a0584d86c
Muh-Baharuddin <4519210022@univpancasila.ac.id>
2023-01-27 22:56:35 +0700

    add useGuard to all nota pembelian route

--- src/nota-pembelian/nota-pembelian.controller.ts
+++ src/nota-pembelian/nota-pembelian.controller.ts
@@ -7,6 +7,7 @@ import {
   Param,
   Delete,
   ParseUUIDPipe,
+  UseGuards,
   UsePipes,
   ValidationPipe,
 } from '@nestjs/common';
@@ -14,28 +15,33 @@ import { NotaPembelianService } from './nota-pembelian.service';
 import { CreateNotaPembelianDto } from './dto/create-nota-pembelian.dto';
 import { UpdateNotaPembelianDto } from './dto/update-nota-pembelian.dto';
 import { NotaPembelian } from './entities/nota-pembelian.entity';
+import { JwtAuthGuard } from 'src/auth/auth.guard';
 
 @Controller('nota-pembelian')
 export class NotaPembelianController {
   constructor(private readonly notaPembelianService: NotaPembelianService) {}
 
   @Get()
+  @UseGuards(JwtAuthGuard)
   findAll() {
     return this.notaPembelianService.findAll();
   }
 
   @Get(':id')
+  @UseGuards(JwtAuthGuard)
   findById(@Param('id', ParseUUIDPipe) id: string): Promise<NotaPembelian> {
     return this.notaPembelianService.findById(id);
   }
 
   @Post()
+  @UseGuards(JwtAuthGuard)
   @UsePipes(ValidationPipe)
   create(@Body() createNotaPembelianDto: CreateNotaPembelianDto) {
     return this.notaPembelianService.create(createNotaPembelianDto);
   }
 
   @Patch(':id')
+  @UseGuards(JwtAuthGuard)
   @UsePipes(ValidationPipe)
   update(
     @Param('id', ParseUUIDPipe) id: string,
@@ -45,6 +51,7 @@ export class NotaPembelianController {
   }
 
   @Delete(':id')
+  @UseGuards(JwtAuthGuard)
   remove(@Param('id') id: string) {
     return this.notaPembelianService.remove(id);
   }
