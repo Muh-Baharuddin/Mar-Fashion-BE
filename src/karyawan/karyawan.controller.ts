f285ab6bccf24d54485fef7674379d1bc619f319
Muh-Baharuddin <4519210022@univpancasila.ac.id>
2023-01-27 22:48:21 +0700

    add useGuard to all karyawan route

--- src/karyawan/karyawan.controller.ts
+++ src/karyawan/karyawan.controller.ts
@@ -7,6 +7,7 @@ import {
   ParseUUIDPipe,
   Patch,
   Post,
+  UseGuards,
   UsePipes,
   ValidationPipe,
 } from '@nestjs/common';
@@ -14,17 +15,20 @@ import { CreateKaryawanDto } from './dto/create-karyawan.dto';
 import { UpdateKaryawanDto } from './dto/update-karyawan.dto';
 import { Karyawan } from './entities/karyawan.entity';
 import { KaryawanService } from './karyawan.service';
+import { JwtAuthGuard } from 'src/auth/auth.guard';
 
 @Controller('karyawan')
 export class KaryawanController {
   constructor(private readonly karyawanService: KaryawanService) {}
 
   @Get()
+  @UseGuards(JwtAuthGuard)
   findAll() {
     return this.karyawanService.findAllKaryawan();
   }
 
   @Get(':id')
+  @UseGuards(JwtAuthGuard)
   findById(@Param('id', ParseUUIDPipe) id: string): Promise<Karyawan> {
     return this.karyawanService.findById(id);
   }
@@ -36,6 +40,7 @@ export class KaryawanController {
   }
 
   @Patch(':id')
+  @UseGuards(JwtAuthGuard)
   @UsePipes(ValidationPipe)
   update(
     @Param('id', ParseUUIDPipe) id: string,
@@ -45,6 +50,7 @@ export class KaryawanController {
   }
 
   @Delete(':id')
+  @UseGuards(JwtAuthGuard)
   remove(@Param('id', ParseUUIDPipe) id: string) {
     return this.karyawanService.removeKaryawan(id);
   }
