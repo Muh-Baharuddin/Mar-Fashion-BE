cb2f35a8b9911296735e549675eee92ec66914ee
Muh-Baharuddin <4519210022@univpancasila.ac.id>
2023-01-27 22:33:11 +0700

    change put to patch for user change password route and add useGuard to all user route

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -2,7 +2,6 @@ import {
   Controller,
   Get,
   Body,
-  Put,
   Param,
   Delete,
   ParseUUIDPipe,
@@ -11,6 +10,7 @@ import {
   UseGuards,
   UseInterceptors,
   ClassSerializerInterceptor,
+  Patch,
 } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UpdateUserDto } from './dto/update-user.dto';
@@ -22,16 +22,18 @@ export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Get()
+  @UseGuards(JwtAuthGuard)
   findAll() {
     return this.usersService.findAll();
   }
 
   @Get(':id')
+  @UseGuards(JwtAuthGuard)
   getById(@Param('id', ParseUUIDPipe) id: string): Promise<User> {
     return this.usersService.getUserById(id);
   }
 
-  @Put(':id/ubah-password')
+  @Patch(':id/ubah-password')
   @UsePipes(ValidationPipe)
   @UseGuards(JwtAuthGuard)
   @UseInterceptors(ClassSerializerInterceptor)
@@ -43,6 +45,7 @@ export class UsersController {
   }
 
   @Delete(':id')
+  @UsePipes(ValidationPipe)
   remove(@Param('id') id: string) {
     return this.usersService.remove(+id);
   }
