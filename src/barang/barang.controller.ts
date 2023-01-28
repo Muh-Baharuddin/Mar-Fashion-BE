a86c8250b6bed3c3e36d453a1e6a80664e0d6b9f
Muh-Baharuddin <4519210022@univpancasila.ac.id>
2023-01-28 16:45:13 +0700

    make UseGuards to all barang controller endpoints

--- src/barang/barang.controller.ts
+++ src/barang/barang.controller.ts
@@ -18,30 +18,27 @@ import { UpdateBarangDto } from './dto/update-barang.dto';
 import { Barang } from './entities/barang.entity';
 
 @Controller('barang')
+@UseGuards(JwtAuthGuard)
 export class BarangController {
   constructor(private readonly barangService: BarangService) {}
 
   @Get()
-  @UseGuards(JwtAuthGuard)
   findAll() {
     return this.barangService.findAllBarang();
   }
 
   @Get(':id')
-  @UseGuards(JwtAuthGuard)
   findById(@Param('id', ParseUUIDPipe) id: string): Promise<Barang> {
     return this.barangService.findById(id);
   }
 
   @Post()
-  @UseGuards(JwtAuthGuard)
   @UsePipes(ValidationPipe)
   create(@Body() createBarangDto: CreateBarangDto) {
     return this.barangService.createBarang(createBarangDto);
   }
 
   @Patch(':id')
-  @UseGuards(JwtAuthGuard)
   @UsePipes(ValidationPipe)
   update(
     @Param('id', ParseUUIDPipe) id: string,
@@ -51,7 +48,6 @@ export class BarangController {
   }
 
   @Delete(':id')
-  @UseGuards(JwtAuthGuard)
   remove(@Param('id', ParseUUIDPipe) id: string) {
     return this.barangService.removeBarang(id);
   }
