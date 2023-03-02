8e921c3c22cda8ad3d9c426c5937f8f5a64fbef8
Muh-Baharuddin <4519210022@univpancasila.ac.id>
2023-03-02 16:04:31 +0700

    change name

--- src/employees/entities/employees.entity.ts
+++ src/employees/entities/employees.entity.ts
@@ -1,17 +1,8 @@
-import {
-  Entity,
-  Column,
-  PrimaryGeneratedColumn,
-  Generated,
-  CreateDateColumn,
-  UpdateDateColumn,
-  OneToMany,
-} from 'typeorm';
-import { Employee_Saving } from './employee_saving.entity';
+import { Entity, Column, PrimaryGeneratedColumn, Generated } from 'typeorm';
 
 @Entity()
 export class Employees {
-  @PrimaryGeneratedColumn('uuid')
+  @PrimaryGeneratedColumn()
   @Generated('uuid')
   id_employee: string;
 
@@ -19,39 +10,8 @@ export class Employees {
   nama: string;
 
   @Column()
-  address: string;
+  alamat: string;
 
   @Column()
-  phone_number: string;
-
-  @Column()
-  entry_date: Date;
-
-  @Column()
-  exit_date: Date;
-
-  @Column()
-  total_saving: number;
-
-  @OneToMany(() => Employee_Saving, saving => saving.employee)
-  savings: Employee_Saving[];
-
-  @CreateDateColumn({ 
-    type: 'timestamp',
-    default: () => 'CURRENT_TIMESTAMP()' ,
-  })
-  created_at: Date;
-
-  @UpdateDateColumn({
-    type: 'timestamp',
-    default: () => 'CURRENT_TIMESTAMP()',
-    onUpdate: 'CURRENT_TIMESTAMP()',
-  })
-  updated_at: Date;
-
-  @Column({ nullable: true })
-  create_by: string;
-
-  @Column({ nullable: true })
-  update_by: string;
+  nomor_telepon: string;
 }
