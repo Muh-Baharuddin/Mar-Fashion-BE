2af98a6dda20543a63dbd3e10600b7bb71113208
Muh-Baharuddin <4519210022@univpancasila.ac.id>
2023-03-03 15:44:47 +0700

    fix supplier entity

--- src/supplier/entities/supplier.entity.ts
+++ src/supplier/entities/supplier.entity.ts
@@ -1,17 +1,45 @@
-import { Entity, Column, PrimaryGeneratedColumn, Generated } from 'typeorm';
+import { Entity, Column, PrimaryGeneratedColumn, Generated, CreateDateColumn, UpdateDateColumn } from 'typeorm';
 
-@Entity()
+@Entity('suppliers')
 export class Supplier {
-  @PrimaryGeneratedColumn()
+  @PrimaryGeneratedColumn('uuid')
   @Generated('uuid')
   id: string;
 
   @Column()
-  nama: string;
+  name: string;
 
   @Column()
-  alamat: string;
+  address: string;
 
   @Column()
-  nomor_telepon: string;
+  city: string;
+
+  @Column()
+  phone_number: string;
+
+  @Column()
+  account_number: string;
+
+  @Column()
+  account_owner: string;
+
+  @CreateDateColumn({ 
+    type: 'timestamp',
+    default: () => 'CURRENT_TIMESTAMP()' ,
+  })
+  created_at: Date;
+
+  @UpdateDateColumn({
+    type: 'timestamp',
+    default: () => 'CURRENT_TIMESTAMP()',
+    onUpdate: 'CURRENT_TIMESTAMP()',
+  })
+  updated_at: Date;
+
+  @Column({ nullable: true })
+  create_by: string;
+
+  @Column({ nullable: true })
+  update_by: string;
 }
