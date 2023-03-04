ec27565546c5d8594748f175224bd3ceee7793cc
Muh-Baharuddin <4519210022@univpancasila.ac.id>
2023-03-04 22:12:27 +0700

    create income entity

--- src/incomes/entities/income.entity.ts
+++ src/incomes/entities/income.entity.ts
@@ -1,6 +1,43 @@
-import { Entity } from "typeorm";
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  Generated,
+  CreateDateColumn,
+  UpdateDateColumn,
+} from 'typeorm';
 
-@Entity()
+@Entity('incomes')
 export class Income {
-  
+  @PrimaryGeneratedColumn('uuid')
+  @Generated('uuid')
+  id: string;
+
+  @Column()
+  date: Date;
+
+  @Column()
+  total: number;
+
+  @Column()
+  description: string;
+
+  @CreateDateColumn({ 
+    type: 'timestamp',
+    default: () => 'CURRENT_TIMESTAMP()' ,
+  })
+  created_at: Date;
+
+  @UpdateDateColumn({
+    type: 'timestamp',
+    default: () => 'CURRENT_TIMESTAMP()',
+    onUpdate: 'CURRENT_TIMESTAMP()',
+  })
+  updated_at: Date;
+
+  @Column({ nullable: true })
+  create_by: string;
+
+  @Column({ nullable: true })
+  update_by: string;
 }
