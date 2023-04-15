d20fe5c315690f59ce2a09775c89e68fe2f10e1d
Muh-Baharuddin <4519210022@univpancasila.ac.id>
2023-04-15 14:50:45 +0700

    create customer complaint entity

--- src/customer_complaint/entities/customer_complaint.entity.ts
+++ src/customer_complaint/entities/customer_complaint.entity.ts
@@ -1,1 +1,25 @@
-export class CustomerComplaint {}
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  Generated,
+} from 'typeorm';
+
+@Entity('customer_complaint')
+export class CustomerComplaint {
+  @PrimaryGeneratedColumn('uuid')
+  @Generated('uuid')
+  id: string;
+
+  @Column({ nullable: true })
+  name: string;
+
+  @Column({ nullable: true })
+  address: string;
+
+  @Column({ nullable: true })
+  city: string;
+
+  @Column()
+  description: string;
+}
